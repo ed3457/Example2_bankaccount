ba195919b848b75dfe01ade657c3de87cf58fbe6
Kamel R <kamel.r@wayne.edu>
2024-01-22 18:41:53 -0500

    Add project files.

---
+++ BankAccount.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class BankAccount
+{
+private:
+	string clientName;
+	char currency;
+	float balance;
+
+public:
+	void setClientName(string name);
+	string getClientName();
+
+	void setCurrency(char c);
+	string getCurrency();
+
+	string getBalance();
+
+	void PrintAccountInfo();
+
+	void withdraw(string amount);
+	void deposit(string amount); 
+
+
+
+};
+
